5f0aa0b34ac8b5eaf9453925d8e4af7b60d56fa9
PedroTsI <88915142+PedroTsI@users.noreply.github.com>
2022-08-30 13:43:37 -0300

    Delete banco-js-1-contas directory

--- banco-js-1-contas/js/modelo/poupanca.js
+++
@@ -1,9 +0,0 @@
-class Poupanca extends Conta {
-    constructor(numero, saldo, dataAniversario) {
-        super(numero, saldo, dataAniversario);
-    }
-
-    incrementarPoupanca(dataAniversario){
-        this.creditar(valor*1.05)
-}
-}
